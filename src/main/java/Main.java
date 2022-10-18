06f386eac6321c193f7d21cf22216bf8b5f22395
Kamil00o <laganowski.kamil@gmail.com>
2022-10-18 23:21:40 +0200

    sideBranch changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] arg) {
         System.out.println("Siema czy to sie juz kompiluje ? ");
         System.out.println("Czy uda się zrobić 1 pulla ? ");
+        System.out.println("sideBranch zmiany");
     }
 }
