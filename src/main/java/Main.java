d8465adfef716358852bee38c30e18dc8628652e
Kamil00o <laganowski.kamil@gmail.com>
2022-10-18 23:50:25 +0200

    New change on main branch before conflict
    Signed-off-by: Kamil00o <laganowski.kamil@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Siema czy to sie juz kompiluje ? ");
         System.out.println("Czy uda się zrobić 1 pulla ? ");
         System.out.println("sideBranch zmiany");
+        System.out.println("Master przed zmiana na nowym branchu");
     }
 }
