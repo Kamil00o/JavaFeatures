20c807c55f1c76c34fcaf1bb6a97cbf9bc0b01b7
Kamil00o <laganowski.kamil@gmail.com>
2022-10-22 21:33:10 +0200

    kolejne zmiany na sidebranchu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] arg) {
         System.out.println("Siema czy to sie juz kompiluje ? ");
-        System.out.println("Czy uda się zrobić 1 pulla ? ");
-        System.out.println("sideBranch zmiany");
-        System.out.println("Master przed zmiana na nowym branchu");
-        System.out.println("zmiany po wczesniejszym commicie na masterze");
-        System.out.println("nowa linai na SideBranchu");
+        System.out.println("ale bedzie konfliktów");
+
     }
 }
