0dc7a5898a6887d2eacdb38d0e492079faec3539
Kamil00o <laganowski.kamil@gmail.com>
2022-10-22 23:45:33 +0200

    nowy feature na sideBranchu
    Signed-off-by: Kamil00o <laganowski.kamil@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] arg) {
         System.out.println("BRANCH 1");
         System.out.println("Nie ogarniam tego xDDD");
+        System.out.println("jakas nowa funkcja");
+        /////jakis nowy feature
     }
 }
