46432e3b8c892718060829c39ed3782bef1e6538
Kamil00o <laganowski.kamil@gmail.com>
2022-10-22 23:43:32 +0200

    zmiany na mainie
    Signed-off-by: Kamil00o <laganowski.kamil@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] arg) {
-        System.out.println("Siema czy to sie juz kompiluje ? ");
-        System.out.println("Czy uda się zrobić 1 pulla ? ");
-        System.out.println("sideBranch zmiany");
-        System.out.println("Master przed zmiana na nowym branchu");
-        System.out.println("zmiany po wczesniejszym commicie na masterze");
         System.out.println("BRANCH 1");
         System.out.println("Nie ogarniam tego xDDD");
     }
